4f604c3f62eb741b7039b2b075397481893ea35c
vivek <vivek@athenaeum.in>
2021-03-17 16:32:02 +0530

    done

---
+++ src/main/java/in/athenaeum/springbootdemo/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package in.athenaeum.springbootdemo.repositories;
+
+import in.athenaeum.springbootdemo.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
