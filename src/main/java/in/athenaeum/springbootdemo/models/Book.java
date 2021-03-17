1375c287af889ca84ed77ac10149534dde66374d
vivek <vivek@athenaeum.in>
2021-03-17 15:27:05 +0530

    done

---
+++ src/main/java/in/athenaeum/springbootdemo/models/Book.java
@@ -0,0 +1,77 @@
+package in.athenaeum.springbootdemo.models;
+
+public class Book {
+    private long id;
+    private String title;
+    private String category;
+    private String author;
+    private double price;
+    private int nPages;
+
+    public Book() {
+    }
+
+    public Book(
+            long id,
+            String title,
+            String category,
+            String author,
+            double price,
+            int nPages
+    ) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.author = author;
+        this.price = price;
+        this.nPages = nPages;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getnPages() {
+        return nPages;
+    }
+
+    public void setnPages(int nPages) {
+        this.nPages = nPages;
+    }
+}
