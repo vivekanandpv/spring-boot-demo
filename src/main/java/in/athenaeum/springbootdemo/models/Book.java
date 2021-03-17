ef195cd1ce02f02a6c1455db4d5e414afc199a73
vivek <vivek@athenaeum.in>
2021-03-17 16:17:10 +0530

    done

--- src/main/java/in/athenaeum/springbootdemo/models/Book.java
+++ src/main/java/in/athenaeum/springbootdemo/models/Book.java
@@ -1,6 +1,14 @@
 package in.athenaeum.springbootdemo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String category;
